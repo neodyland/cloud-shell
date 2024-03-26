2d4fcb28bfec3c45fcd9741e508ab9cd80018d67
コマリン親衛隊 <masato190411@icloud.com>
2024-03-26 09:59:55 +0900

    Create basic.ts

---
+++ tests/basic.ts
@@ -0,0 +1,22 @@
+import WebSocket from "ws";
+
+const ws = new WebSocket("ws://localhsot:8000/ws");
+
+ws.on('error', console.error);
+
+ws.on('open', function open() {
+  ws.send('something');
+});
+
+ws.on('message', function message(data) {
+  console.log('received: %s', data);
+  const payload = JSON.parse(data);
+  if (payload.op === 2) {
+    ws.send(JSON.stringify({
+      op: 3,
+      data: "pacman -Syyu --noconfirm",
+    });
+  } else if (payload.op === 4) {
+    console.log(payload.data);
+  };
+});
